fa334093dddf9c0dae6e562a45a333d9fa55d3d2
anwarr <a.ammarr17@gmail.com>
2023-12-06 20:52:11 +0100

    add classes

---
+++ demo/src/main/java/com/example/models/chef_stock.java
@@ -0,0 +1,33 @@
+package com.example.models;
+
+import java.util.Date;
+
+/**
+ * chef_stock
+ */
+public class chef_stock {
+
+    String choix_service;
+    Date date;
+
+    public chef_stock(String choix_service, Date date) {
+        this.choix_service = choix_service;
+        this.date = date;
+    }
+
+    public String getChoix_service() {
+        return this.choix_service;
+    }
+
+    public void setChoix_service(String choix_service) {
+        this.choix_service = choix_service;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
